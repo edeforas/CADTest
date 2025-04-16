f70af34757894108ea867e8ee771b88e1960dca6
Etienne de Foras <etienne.deforas@gmail.com>
2025-04-16 19:49:53 +0200

    more wip

--- src/StepFile.cpp
+++ src/StepFile.cpp
@@ -8,80 +8,8 @@ using namespace std;
 #include "NurbsSolid.h"
 
 namespace StepFile
-{   /*
-    class StepLine
-    {
-    public:
-        StepLine(const string& s)
-        {
-            from_string(s);
-        }
+{
 
-        void from_string(const string& s)
-        {
-            if (s[0] != '#')
-                return;
-            int iPosEqual = s.find('=');
-            _id = atoi(s.substr(1, iPosEqual).c_str());
-
-        }
-
-        int _id;
-        //string _type;
-    };
-    
-    bool save(const string& filename, const Body& body)
-    {
-        ofstream f(filename);
-        if (!f)
-            return false;
-
-        return false;
-    }
-    ///////////////////////////////////////////////////////////////////////////
-	bool load(const string& filename, Body& body)
-    {
-        body.clear();
-        ifstream f(filename);
-        if (!f)
-            return false;
-
-        vector<StepLine> vs;
-
-        bool bDataReady = false;
-
-        // read all lines
-        while (!f.eof())
-        {
-            string sLine; //,sType;
-            getline(f, sLine);
-
-            if (sLine.size() == 0)
-                continue;
-
-            //skip header
-            if (sLine == "DATA;")
-                bDataReady = true;
-            if (bDataReady == false)
-                continue;
-
-            //read full line
-            while (sLine[sLine.size() - 1] != ';')
-            {
-                string s;
-                if (f.eof())
-                    return false;
-                getline(f, s);
-                sLine += s;
-            }
-
-            vs.push_back(StepLine(sLine));
-        }
-
-        return true;
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    */
 }
 
 StepWriter::StepWriter():
@@ -93,8 +21,6 @@ void StepWriter::write_header()
 {
     _f << "ISO-10303-21;" << endl;
     _f << "HEADER;" << endl;
-    //_f << "FILE_DESCRIPTION((''),'2;1');" << endl;
-    //_f << "FILE_NAME('" << _sNameFile << "','',(''),(''), '', 'CADTest', '');" << endl;
     _f << "FILE_SCHEMA(('AUTOMOTIVE_DESIGN { 1 0 10303 214 3 1 1 }'));" << endl;
 	_f << "ENDSEC;" << endl << endl;
 
@@ -239,7 +165,7 @@ void StepWriter::write(const NurbsSurface& n)
 	_iItemIndex++;
 	_f << "#" << _iItemIndex + 1 << "=ADVANCED_BREP_SHAPE_REPRESENTATION('',(#" << _iItemIndex << "),'');" << endl;
 	_iItemIndex++;
-	_f << "#" << _iItemIndex + 1 << "=SHAPE_REPRESENTATION_RELATIONSHIP('','None',#100002,#" << _iItemIndex << ");" << endl << endl;
+	_f << "#" << _iItemIndex + 1 << "=SHAPE_REPRESENTATION_RELATIONSHIP('','None',#10002,#" << _iItemIndex << ");" << endl << endl;
 	_iItemIndex++;
 
 
